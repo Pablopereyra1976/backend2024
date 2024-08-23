0b303773499caa94c7ed3d8703f9f6ede3ebe7cc
Pablo Pereyra <143567596+Pablopereyra1976@users.noreply.github.com>
2024-08-23 00:56:45 -0300

    arreglos en clase4 y clase5

--- clase5/ejercicio.js
+++ clase5/ejercicio.js
@@ -10,6 +10,20 @@ class Empleado {
     descripcion() {
         return `Empleado(ID: ${this.idEmpleado}, Nombre: ${this.nombre}, puesto: ${this.puesto})`;
     }
+    presentarse(nombre) {
+        console.log("hola " + nombre + " soy " + this.nombre + ' y trabajo como ' + this.puesto);
+    }
+}
+class Pasante extends Empleado {
+    constructor(nombre, sueldo, fechaContratacion, idEmpleado, puesto, tiempoDeContratoEnMeses) {
+        super(nombre, sueldo, fechaContratacion, idEmpleado, 'Developer');
+        this.tiempoDeContratoEnMeses = tiempoDeContratoEnMeses;
+    }
+    hacerCosasDePasante() {
+        if (this.puesto === 'Developer') {
+            console.log('Estoy programando');
+        }
+    }
 }
 class ManejadorEmpleados {
     constructor(idManejador) {
@@ -27,8 +41,17 @@ class ManejadorEmpleados {
         return this.empleados.filter(empleado => empleado.puesto === puesto);
     }
 }
-const manejador = new ManejadorEmpleados(1);
+const Pasante1 = new Pasante("Pedro Lopez", 50000, "2022-01-15", 1, "Developer", 6);
+const empleado4 = new Empleado("Sofía Vega", 80000, "2022-01-15", 4, "Marketing");
 
+
+Pasante1.presentarse('pedro');
+empleado4.presentarse('sofia');
+Pasante1.hacerCosasDePasante();
+
+
+
+const manejador = new ManejadorEmpleados(1);
 
 
 // Crear empleados
@@ -37,21 +60,15 @@ const empleado2 = new Empleado("Ana López", 60000, "2021-05-20", 2, "Designer")
 const empleado3 = new Empleado("Carlos Ruiz", 70000, "2020-09-30", 3, "Project Manager");
 
 
-
-
 // Agregar empleados al manejador
 manejador.agregarEmpleado(empleado1);
 manejador.agregarEmpleado(empleado2);
 manejador.agregarEmpleado(empleado3);
 
 
-
-
 // Obtener un empleado por ID
 const empleado = manejador.obtenerEmpleadoPorId(2);
 console.log(empleado === null || empleado === void 0 ? void 0 : empleado.descripcion()); // Debería imprimir la información de Ana López
-
-
 
 
 // Obtener empleados por tipo
--- clase5/ejercicio.ts
+++ clase5/ejercicio.ts
@@ -5,8 +5,9 @@ class Empleado {
     idEmpleado: number;
     puesto: 'Project Manager' | 'Developer' | 'Designer' | 'Marketing';
 
-    constructor(nombre: string, sueldo: number, fechaContratacion: string, idEmpleado: number, puesto: 'Project Manager' | 'Developer' | 'Designer' | 'Marketing') 
-       { this.nombre = nombre,
+
+    constructor(nombre: string, sueldo: number, fechaContratacion: string, idEmpleado: number, puesto: 'Project Manager' | 'Developer' | 'Designer' | 'Marketing') {
+        this.nombre = nombre,
         this.sueldo = sueldo,
         this.fechaContratacion = fechaContratacion,
         this.idEmpleado = idEmpleado,
@@ -14,12 +15,30 @@ class Empleado {
     }
     descripcion(): string {
         return `Empleado(ID: ${this.idEmpleado}, Nombre: ${this.nombre}, puesto: ${this.puesto})`;
+    }
+
+
+    presentarse(nombre: string) {
+        console.log("hola " + nombre + " soy " + this.nombre + ' y trabajo como ' + this.puesto);
+    }
+}
+class Pasante extends Empleado {
+    tiempoDeContratoEnMeses: number;
+    constructor(nombre: string, sueldo: number, fechaContratacion: string, idEmpleado: number, puesto: 'Project Manager' | 'Developer' | 'Designer' | 'Marketing', tiempoDeContratoEnMeses: number) {
+        super(nombre, sueldo, fechaContratacion, idEmpleado, 'Developer');
+        this.tiempoDeContratoEnMeses = tiempoDeContratoEnMeses;
+    }
+    hacerCosasDePasante(){
+        if(this.puesto === 'Developer'){
+            console.log('Estoy programando');
+        }
+        }
+
+   
 }
 
-}	
 
 
-    
 class ManejadorEmpleados {
     idManejador: number;
     empleados: Empleado[];
@@ -42,6 +61,12 @@ class ManejadorEmpleados {
         return this.empleados.filter(empleado => empleado.puesto === puesto);
     }
 }
+const Pasante1 = new Pasante("Pedro Lopez", 50000, "2022-01-15", 1, "Developer", 6);
+const empleado4 = new Empleado("Sofía Vega", 80000, "2022-01-15", 4, "Marketing");
+
+Pasante1.presentarse('pedro');
+empleado4.presentarse('sofia');
+Pasante1.hacerCosasDePasante();
 
 const manejador = new ManejadorEmpleados(1);
 
@@ -51,7 +76,6 @@ const manejador = new ManejadorEmpleados(1);
 const empleado1 = new Empleado("Juan Pérez", 50000, "2022-01-15", 1, "Developer");
 const empleado2 = new Empleado("Ana López", 60000, "2021-05-20", 2, "Designer");
 const empleado3 = new Empleado("Carlos Ruiz", 70000, "2020-09-30", 3, "Project Manager");
-
 
 
 
